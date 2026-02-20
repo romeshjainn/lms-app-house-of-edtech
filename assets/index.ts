4619b8606b01886a5ddcba32d5b3ac04e97658d5
Romesh Jain <romeshjainofficial@gmail.com>
2026-02-20 14:29:29 +0530

    Initialized the Expo project

---
+++ assets/index.ts
@@ -0,0 +1,14 @@
+
+export const IMAGES = {
+  androidIconBack: require('./images/android-icon-background.png'),
+  androidIconForeground: require('./images/android-icon-foreground.png'),
+  androidIconMono: require('./images/android-icon-monochrome.png'),
+
+  favicon: require('./images/favicon.png'),
+  appIcon: require('./images/icon.png'),
+  splash: require('./images/splash-icon.png'),
+
+  reactLogo: require('./images/react-logo.png'),
+  partialReactLogo: require('./images/partial-react-logo.png'),
+} as const;
+
